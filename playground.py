2bb1e52dd94ee5b040e2ed37412784fad352f8cd
Radical-Radish <64011688@kmitl.ac.th>
2023-04-28 18:24:41 +0700

    webcam camera usage

--- playground.py
+++ playground.py
@@ -6,8 +6,8 @@ from Functions_Copy import *
 
 i = 1
 
-opencam()
-img = cv2.imread(r"C:\Users\tcsuy\Documents\GitHub\doccument-scanner\captured_image.jpg", cv2.IMREAD_ANYCOLOR)
+# opencam()
+img = cv2.imread(r"C:\Users\tcsuy\Documents\GitHub\doccument-scanner\captured_image1.jpg", cv2.IMREAD_ANYCOLOR)
 
 img, original = resize(img)
 blank = blankpage(img)
@@ -18,15 +18,7 @@ desCoor, maxWidth, maxHeight = desCoordinates(corners)
 result = warp_Per(corners, desCoor, original, maxWidth, maxHeight)
 
 plt.figure(figsize = (10,7))
-plt.imshow(result)
+plt.imshow(con)
 plt.axis("off")
-while True:
-    path = (r"C:\Users\tcsuy\Documents\GitHub\doccument-scanner\result_%d.png") %i
-    if os.path.isfile(path) is True:
-        i += 1
-    else:
-        cv2.imwrite("result_%d.png" %i, result)
-        break
-
 plt.title("Scanned picture")
 plt.show()
